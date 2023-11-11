53a34390c343a61939d44cb3aa1c27ffc8365acc
Cx330-502 <1175963382@qq.com>
2023-11-11 22:30:25 +0800

    更新 hexo_mermaid_converter.py

--- hexo_mermaid_converter.py
+++ hexo_mermaid_converter.py
@@ -1,4 +1,32 @@
+import os
 
+
+def convert_hexo_mermaid_code_to_markdown_mermaid_code(file_path):
+    content = ''
+    with open(file_path, 'r', encoding='utf-8') as f:
+        content = f.read()
+    content = content.replace('{% mermaid %}', '```mermaid')
+    content = content.replace('{% endmermaid %}', '```')
+    with open(file_path, 'w', encoding='utf-8') as f:
+        f.write(content)
+
+
+def convert_markdown_mermaid_code_to_hexo_mermaid_code(file_path):
+    content = ''
+    with open(file_path, 'r', encoding='utf-8') as f:
+        content = f.read()
+    flag = False
+    for i in range(0, len(content)):
+        if content[i] == '`' and content[i + 1] == '`' and content[i + 2] == '`':
+            content = content[:i] + '{% mermaid %}' + content[i + 3:]
+            flag = True
+            break
+        if content[i] == '`' and content[i + 1] == '`' and content[i + 2] == '`' and content[i + 3] == '\n' and flag:
+            content = content[:i] + '{% endmermaid %}' + content[i + 3:]
+            flag = False
+            break
+    with open(file_path, 'w', encoding='utf-8') as f:
+        f.write(content)
 
 
 if __name__ == '__main__':
@@ -7,3 +35,15 @@ if __name__ == '__main__':
     print("2. Convert markdown mermaid code to hexo mermaid code")
     mode = input("Please input the mode number: ")
     if mode == '1':
+        for root, dirs, files in os.walk('./source/_posts'):
+            for file in files:
+                if file.endswith('.md'):
+                    print("Converting file: " + file)
+                    convert_hexo_mermaid_code_to_markdown_mermaid_code(os.path.join(root, file))
+
+    elif mode == '2':
+        for root, dirs, files in os.walk('./source/_posts'):
+            for file in files:
+                if file.endswith('.md'):
+                    print("Converting file: " + file)
+                    convert_markdown_mermaid_code_to_hexo_mermaid_code(os.path.join(root, file))
