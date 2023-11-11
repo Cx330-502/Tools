7e3018e7ba4e15e77181cc3aa961da0d8abeb3e4
Cx330-502 <1175963382@qq.com>
2023-11-11 22:24:42 +0800

    创建 hexo_mermaid_converter.py

---
+++ hexo_mermaid_converter.py
@@ -0,0 +1,9 @@
+
+
+
+if __name__ == '__main__':
+    print("Please choose the mode of the converter: ")
+    print("1. Convert hexo mermaid code to markdown mermaid code")
+    print("2. Convert markdown mermaid code to hexo mermaid code")
+    mode = input("Please input the mode number: ")
+    if mode == '1':
